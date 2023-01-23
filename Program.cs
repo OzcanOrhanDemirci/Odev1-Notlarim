5886973a3a83634347d9370e23d4ba41c9235c5e
Ozcan Orhan Demirci <ozcanorhandemirci@ogr.iu.edu.tr>
2023-01-24 00:51:14 +0300

    Oyun ve Uygulama Akademisi Odev1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhabalar. Kendimi HelloGitHub projem ile tanıtmıştım. Bu konsol uygulamasını ise Akademi GitHub Ödevim için oluşturdum.");
+            Console.WriteLine("Hakkıma detaylı bilgi almak için HelloGitHub Repomu inceleyebilirsiniz. Teşekkürler...");
+            Console.ReadLine();
+        }
+    }
+}
