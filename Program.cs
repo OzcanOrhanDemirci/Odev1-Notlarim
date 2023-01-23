c15bbfec9401b8f9258c2ba8efd39d925a295505
Ozcan Orhan Demirci <ozcanorhandemirci@ogr.iu.edu.tr>
2023-01-24 00:58:55 +0300

    Bilerek yaptigim bir hatayi duzelttim

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Odev1
         static void Main(string[] args)
         {
             Console.WriteLine("Merhabalar. Kendimi HelloGitHub projem ile tanıtmıştım. Bu konsol uygulamasını ise Akademi GitHub Ödevim için oluşturdum.");
-            Console.WriteLine("Hakkıma detaylı bilgi almak için HelloGitHub Repomu inceleyebilirsiniz. Teşekkürler...");
+            Console.WriteLine("Hakkımda detaylı bilgi almak için HelloGitHub Repomu inceleyebilirsiniz. Teşekkürler...");
             Console.ReadLine();
         }
     }
